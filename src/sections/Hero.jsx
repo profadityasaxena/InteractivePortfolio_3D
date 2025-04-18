466c0046e952105cee91e411f96a8af92b421f89
Aditya Saxena <iam_adisxn@outlook.com>
2025-04-18 15:58:10 -0400

    developing the hero

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Hero = () => {
     return (
-        <div>Hero</div>
+        <section id="hero" className="relative overflow-hidden">
+            <div className="absolute top-0 left-0 z-10">
+                <img src="/images/bg.png" alt="" />
+            </div>
+            <div className="hero-layout">
+                {/*Left Side : Hero Left Side Content*/}
+                {/*Right Side : Hero Right Side Content*/}
+            </div>
+
+        </section>
     )
 }
 export default Hero
