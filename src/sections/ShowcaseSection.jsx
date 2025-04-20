c23703d29c7cde3ec20223c3a297c9e01cb0f801
Aditya Saxena <iam_adisxn@outlook.com>
2025-04-20 17:56:31 -0400

    added Bajaj MES project

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const ShowcaseSection = () => {
     return (
-        <div>ShowcaseSection</div>
+        <div id="work" className="app-showcase">
+            <div className="w-full">
+                <div className="showcaselayout">
+                    {/*LEFT SIDE*/}
+                    <div className="first-project-wrapper">
+                        <div className="image-wrapper">
+                            <img src="/images/project1.png" alt="Ryde"/>
+
+                        </div>
+                    </div>
+                    {/*RIGHT SIDE*/}
+                </div>
+            </div>
+        </div>
     )
 }
 export default ShowcaseSection
