658179f4412233d740f2194fe3b7038b89e64464
Aditya Saxena <iam_adisxn@outlook.com>
2025-04-20 16:29:14 -0400

    adding Animated Counter

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -4,6 +4,7 @@ import Button from "../components/Button.jsx";
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
 const Hero = () => {
     useGSAP(() =>{
@@ -58,6 +59,7 @@ const Hero = () => {
                     </div>
                 </figure>
             </div>
+            <AnimatedCounter/>
 
         </section>
     )
