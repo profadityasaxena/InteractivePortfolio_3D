8b00f5b0a1e0f8c37e797b0c1af6bc376289c4f9
Aditya Saxena <iam_adisxn@outlook.com>
2025-04-20 15:55:45 -0400

    adding particles effect

--- src/components/HeroModels/HeroLights.jsx
+++ src/components/HeroModels/HeroLights.jsx
@@ -4,9 +4,9 @@ const HeroLights = () => {
     return (
         <>
             <ambientLight intensity={2} color="green" />
-            <directionalLight position={[3, 3, 3]} intensity={2} color="white"/>
-            <spotLight position={[2, -2, 2]} intensity={5} angle={10} penumbra={1} color="red" />
-            <spotLight position={[2, -4, 6]} intensity={113} angle={10} penumbra={1} color="blue" />
+            <directionalLight position={[3, 3, 3]} intensity={50} color="white"/>
+            <spotLight position={[2, -2, 2]} intensity={20} angle={10} penumbra={1} color="red" />
+            <spotLight position={[2, -4, 6]} intensity={11} angle={10} penumbra={1} color="blue" />
             <pointLight position={[0, 0, 0]} intensity={10} color="violet" />
         </>
     )
