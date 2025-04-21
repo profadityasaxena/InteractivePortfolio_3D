3334003a7456a4b8916bc3596e25ef6e34d1204d
Aditya Saxena <iam_adisxn@outlook.com>
2025-04-20 22:43:59 -0400

    added animation to the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Hero from './sections/Hero.jsx'
 import ShowcaseSection from "./sections/ShowcaseSection.jsx";
 import CustomCursor from "./components/CustomCursor.jsx";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
     return (
         <>
             <CustomCursor />
+            <Navbar/>
             <Hero />
             <ShowcaseSection />
         </>
